dc4eb5bb8a640f31ee14491e11841885126072ef
Dan <icarlosdan8@gmail.com>
2021-10-20 15:58:52 -0500

    131. Ejercicios AJAX - APIs: Pagos Online con Fetch y Stripe (3/4)

--- assets/stripe-checkout.js
+++ assets/stripe-checkout.js
@@ -5,36 +5,48 @@ import STRIPE_KEYS from "./stripe-keys.js";
 const d = document,
 	$tacos = d.getElementById("tacos"),
 	$template = d.getElementById("taco-template").content,
-	$fragment = d.createDocumentFragment();
+	$fragment = d.createDocumentFragment(),
+	fetchOptions = {
+		headers: {
+			Authorization: `Bearer ${STRIPE_KEYS.private}`,
+		},
+	};
+let prices, products;
 
-fetch("https://api.stripe.com/v1/products", {
-	headers: {
-		Authorization: `Bearer ${STRIPE_KEYS.private}`,
-	},
-})
-	.then((res) => {
-		console.log(res);
-		return res.json();
-	})
+Promise.all([
+	fetch("https://api.stripe.com/v1/products", fetchOptions),
+	fetch("https://api.stripe.com/v1/prices", fetchOptions),
+])
+	.then((responses) => Promise.all(responses.map((res) => res.json())))
 	.then((json) => {
-		console.log(json);
+		// console.log(json[0].data);
+		products = json[0].data;
+		prices = json[1].data;
+
+		prices.forEach((el) => {
+			let productData = products.filter(
+				(product) => product.id === el.product
+			);
+			console.log(productData);
+
+			$template.querySelector(".taco").setAttribute("data-price", el.id);
+			$template.querySelector("img").src = productData[0].images[0];
+			$template.querySelector("img").alt = productData[0].name;
+			$template.querySelector("figcaption").innerHTML = `
+                ${productData[0].name}
+                <br>
+                $${el.unit_amount_decimal} ${el.currency}
+                `;
+
+			let $clone = d.importNode($template, true);
+			$fragment.appendChild($clone);
+		});
+		$tacos.appendChild($fragment);
 	})
 	.catch((err) => {
-		console.error(err);
-	});
-
-fetch("https://api.stripe.com/v1/prices", {
-	headers: {
-		Authorization: `Bearer ${STRIPE_KEYS.private}`,
-	},
-})
-	.then((res) => {
-		console.log(res);
-		return res.json();
-	})
-	.then((json) => {
-		console.log(json);
-	})
-	.catch((err) => {
-		console.error(err);
+		console.log(err);
+		let message =
+			err.statusText ||
+			"Ocurrió un error al conectarse con la API de Stripe";
+		$tacos.innerHTML = `<p>Error ${err.status}: ${message}`;
 	});
